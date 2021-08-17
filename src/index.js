d679ff0eb2777473480ad77879f692ed6c912f4a
Dmitriy Bartashevich <3088742@mail.ru>
2021-08-17 16:40:53 +0300

    add result

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
 exports.min = function min (array) {
-  return 0;
+	if (!array || 0 === array.length) return 0
+	let result = array.sort((a, b) => a - b);
+	return result[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+	if (!array || 0 === array.length) return 0
+	let result = array.sort((a, b) => b - a);
+	return result[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+	if (!array || 0 === array.length) return 0
+	let result = array.reduce((a, b) => a + b) / array.length;
+	return result;
 }
